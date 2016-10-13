471b6d25e734cb3451a0c9dffb75fbdfad585f98
iamanam <iamanam4@gmail.com>
2016-10-14 02:20:19 +0600

    some changes to revslider and visual-composer, some new plugins add wp-rocket, wp-ithmes-security wp-seo

---
+++ content/wp-rocket-config/hostbybest.com.php
@@ -0,0 +1,14 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+$rocket_cookie_hash = '5bf64a0567e4cc9da8331c2e30649289';
+$rocket_secret_cache_key = '57f28e5e7582c221470224';
+$rocket_cache_mobile = '1';
+$rocket_cache_reject_uri = '.*/feed/?|/wc-api/v(.*)|/wp-json/(.*)';
+$rocket_cache_reject_cookies = 'wp-postpass_|wptouch_switch_toggle|comment_author_|comment_author_email_';
+$rocket_cache_query_strings = array (
+);
+$rocket_cache_reject_ua = 'facebookexternalhit|FB_IAB|FB4A|FBAV';
+$rocket_cache_mandatory_cookies = '';
+$rocket_cache_dynamic_cookies = array (
+);
