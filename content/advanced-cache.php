2006a6851f3a05991e3cd0c9be9fc1233204e373
iamanam <iamanam4@gmail.com>
2016-10-14 02:35:55 +0600

    some chages

--- content/advanced-cache.php
+++ content/advanced-cache.php
@@ -2,11 +2,11 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 define( 'WP_ROCKET_ADVANCED_CACHE', true );
-$rocket_cache_path = 'D:\php\htdocs\hostbybest.dev/content/cache/wp-rocket/';
-$rocket_config_path = 'D:\php\htdocs\hostbybest.dev/content/wp-rocket-config/';
+$rocket_cache_path = 'D:\www\htdocs\hostbybest.dev/content/cache/wp-rocket/';
+$rocket_config_path = 'D:\www\htdocs\hostbybest.dev/content/wp-rocket-config/';
 
-if ( file_exists( 'D:\php\htdocs\hostbybest.dev\content\plugins\wp-rocket\inc\front/process.php' ) ) {
-	include( 'D:\php\htdocs\hostbybest.dev\content\plugins\wp-rocket\inc\front/process.php' );
+if ( file_exists( 'D:\www\htdocs\hostbybest.dev\content\plugins\wp-rocket\inc\front/process.php' ) ) {
+	include( 'D:\www\htdocs\hostbybest.dev\content\plugins\wp-rocket\inc\front/process.php' );
 } else {
 	define( 'WP_ROCKET_ADVANCED_CACHE_PROBLEM', true );
 }
